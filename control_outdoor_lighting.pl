33e96f71b1d6112fb08255ee41b8f626c2f85e24
Lester Hightower <lester.hightower@gmail.com>
2025-03-27 12:43:23 -0400

    Fixed override bug via a significant code restructuring

--- control_outdoor_lighting.pl
+++ control_outdoor_lighting.pl
@@ -5,7 +5,7 @@
 # time at that location.
 #
 # It is intended to run frequently from cron, using a script like this:
-# 
+#
 # #!/bin/bash
 #
 # DIR="/path/to/the/program"
@@ -26,16 +26,18 @@
 use strict;
 no warnings "experimental::signatures";
 use feature qw(signatures);
-use LWP::UserAgent;
-use IO::Socket::SSL;
-use HTTP::Request::Common;
-use JSON::XS;
-use Time::Piece;
-use Time::Seconds;
-use File::Basename qw(basename);
-use File::Slurp qw(read_file write_file);
-use Data::Dumper;
+use LWP::UserAgent;                       # libwww-perl
+use IO::Socket::SSL;                      # libio-socket-ssl-perl
+use HTTP::Request::Common;                # libhttp-message-perl
+use JSON::XS;                             # libjson-xs-perl
+use Time::Piece;                          # core
+use Time::Seconds;                        # core
+use File::Basename qw(basename);          # core
+use File::Slurp qw(read_file write_file); # core
+use Data::Dumper;                         # core
 $Data::Dumper::Indent = 1;
+#use Data::Printer; # libdata-printer-perl Nicer for Time::Piece object dumps
+#sub PDumper { np @_, colored => 1 } # a Data::Printer PDumper() subroutine
 
 my $CONF = load_conf_file(); # Load configuration
 
@@ -52,101 +54,149 @@ my $ua = LWP::UserAgent->new(
     },
   );
 
+# Calculate the needed lights on and off times
 my $mins_from_sunset = get_mins_from_sunset($CONF->{location});
-print "mins_from_sunset = ".$mins_from_sunset."\n";
+my $TIMES = {mins_from_sunset => $mins_from_sunset};
+{
+  my $now = Time::Piece->localtime; # Needed throughout this scope
+  $TIMES->{now} = $now;
+  my $sunset_time = get_sunset_time_with_cache($now, $CONF->{location});
+  #my $sunset_time = '2025-03-27T10:25:06-04:00'; # For DEVELOPMENT only
 
-# Figure out the needed state that we should be in
-my $needed_state = undef;
-if ($mins_from_sunset <= $CONF->{on_off}->{mins_prior_sunset} &&
-	$mins_from_sunset > -1*$CONF->{on_off}->{mins_after_sunset}) {
-  $needed_state = 1;
-} elsif ($mins_from_sunset <= -1*$CONF->{on_off}->{mins_after_sunset}) {
-  $needed_state = 0;
+  my $sst = convert_sunset_time_to_timepiece($now, $sunset_time);
+  $TIMES->{sunset} = $sst;
+  # Turn lights on relative to mins_prior_sunset config
+  $TIMES->{time_on} = $sst - $CONF->{on_off}->{mins_prior_sunset}*60;
+  # Turn lights off relative to mins_after_sunset config
+  $TIMES->{time_off} = $sst + $CONF->{on_off}->{mins_after_sunset}*60;
+  # Adjust time_off if always_off_by exists and is prior to mins_after_sunset
+  if (exists($CONF->{on_off}->{always_off_by})) {
+    my $always_off_time = $now->datetime;
+    $always_off_time =~ s/\d\d:\d\d:\d\d$/$CONF->{on_off}->{always_off_by}:00/;
+    my $always_off_by = $now->strptime($always_off_time,"%Y-%m-%dT%H:%M:%S");
+    if ($always_off_by->epoch < $TIMES->{time_off}) {
+      $TIMES->{time_off} = $always_off_by;
+    }
+  }
 }
-$needed_state = $CONF->{on_off}->{default_state} if (! defined($needed_state));
+print "Calculated times: ".JDumper($TIMES)."\n";
 
-# We may need to override $needed_state=1 via always_off_by
-if ($needed_state && exists($CONF->{on_off}->{always_off_by})) {
-  #always_off_by => '22:00:00', # Always off by this time
-  my $now = Time::Piece->localtime;
-  # Put the always_off_by into the now time string and parse it
-  my $always_off_time = $now->datetime;
-  $always_off_time =~ s/\d\d:\d\d:\d\d$/$CONF->{on_off}->{always_off_by}:00/;
-  my $always_off_by = $now->strptime($always_off_time,"%Y-%m-%dT%H:%M:%S");
-  print Dumper({now => $now->datetime, off_by => $always_off_by->datetime});
-  # If we've passed the always_off_by time, then needed_state=0
-  if ($now->epoch > $always_off_by->epoch) {
-    print "Overriding due to always_off_by=$CONF->{on_off}->{always_off_by}\n";
-    print Dumper({now => $now->epoch, always_off_by => $always_off_by->epoch});
+# Figure out the needed state that we should be in and apply it
+{
+  my $now = Time::Piece->localtime; # Needed throughout this scope
+
+  # Grab when and what this script last set the outlet state to
+  my $state = get_state_data(); # Our last known state
+  my $last_set_state = $state->{last_set_state} // undef;
+  my $last_set_state_time = $state->{last_set_state_time} // undef;
+  if (defined($last_set_state_time)) {
+    $last_set_state_time = $now->strptime($last_set_state_time, "%Y-%m-%dT%H:%M:%S");
+  }
+
+  # Determine the state things should be in now
+  my $needed_state = undef;
+  if ($now >= $TIMES->{time_on} && $now < $TIMES->{time_off}) {
+    $needed_state = 1;
+  } else {
     $needed_state = 0;
   }
-}
 
-my $current_state = get_plug_state($CONF->{plug_hostname});
-print Dumper({needed_state => $needed_state, current_state => $current_state})."\n";
+  my $current_state = get_plug_state($CONF->{plug_hostname});
+  print JDumper({needed_state => $needed_state, current_state => $current_state})."\n";
 
-# If the current state is not what we decided that we needed, we need to
-# determine if it was overridden and, if so, respect the override_mins.
-#
-# TODO - do we need to keep track of the last state we see and its time...?
-#
-{
-  my $state = get_state_data();
-  if ($current_state != $needed_state && exists($state->{last_set_state_time})) {
-    my $now = Time::Piece->localtime;
-    my $set = $now->strptime($state->{last_set_state_time}, "%Y-%m-%dT%H:%M:%S");
-    my $mins_since_set = int(($now->epoch - $set->epoch) / 60);
-    my $override_mins = $CONF->{on_off}->{override_mins}->{$current_state};
-
-    if (exists($state->{override_start_time})) {
-      my $override_time = $now->strptime($state->{override_start_time}, "%Y-%m-%dT%H:%M:%S");
-      my $mins_since_override = int(($now->epoch - $override_time->epoch) / 60);
-      if ($mins_since_override >= 0 && $mins_since_override <= $override_mins) {
-        print "$mins_since_override mins since we saw an override is within override_mins.\n";
-        print "Not changing state...\n";
-        $needed_state = $current_state;
+  # If we are out of correspondence, analyze the details
+  my $are_we_in_an_override = 0;
+  if ($current_state != $needed_state) {
+    if (! defined($last_set_state_time)) {
+      #print "We have no last_set_state_time and so need to toggle the state\n";
+    } elsif (
+      # If we last set state to on and after the start of the $TIMES->{time_on}
+      # period but it is off now, or if we last set state to off before the
+      # start of the $TIMES->{time_on} or after the start of $TIMES->{time_off}
+      # but it is on now, assume that a user overrode us.
+	($needed_state == 1 && $last_set_state == 1 &&
+			$last_set_state_time->epoch >= $TIMES->{time_on}->epoch)
+	|| ($needed_state == 0 && $last_set_state == 0 &&
+			$last_set_state_time->epoch < $TIMES->{time_on}->epoch)
+	|| ($needed_state == 0 && $last_set_state == 0 &&
+			$last_set_state_time->epoch >= $TIMES->{time_off}->epoch)
+	) {
+      # If we haven't yet recorded an override_start_time, do that now
+      if (! exists($state->{override_start_time})) {
+        $state->{override_start_time} = $now->datetime; # When we first saw this override
+        $state->{override_state} = $current_state; # What state the override is in
+        set_state_data($state);
       }
+      $are_we_in_an_override = 1;
+      #print "We are in an override period and require further analysis\n";
     } else {
-      if ($mins_since_set >= 0 && $mins_since_set <= $override_mins) {
-        print "$mins_since_set mins since our set is within override_mins.\n";
-        print "Not changing state...\n";
-        $needed_state = $current_state;
-      }
-      $state->{override_start_time} = $now->datetime; # When we first saw this override
+      #print "We are not in an override and need to toggle the state\n";
+    }
+  } else {
+    print "We are in correspondence so there is nothing to do.\n";
+    # If we are in correspondence we need to remove any override state info
+    if (exists($state->{override_start_time})) {
+      print "Removing override_start_time of $state->{override_start_time}...\n";
+      delete($state->{override_start_time});
+      delete($state->{override_state});
       set_state_data($state);
     }
-  } elsif (exists($state->{override_start_time})) {
-    # If we stored an override_start_time and now current_state==needed_state
-    # then we should remove that override_start_time
-    print "Removing override_start_time of $state->{override_start_time}...\n";
-    delete($state->{override_start_time});
-    set_state_data($state);
   }
-}
 
-# If we need to change the state of the plug, do that here.
-if ($current_state != $needed_state) {
-  print "Change needed current_state=$current_state to needed_state=$needed_state\n";
-  set_plug_state($CONF->{plug_hostname}, $needed_state);
-  my $new_state = get_plug_state($CONF->{plug_hostname});
-  if ($new_state != $needed_state) {
-    warn "get_plug_state() failed to set the net state!\n";
-  } else {
-    save_plug_state($new_state);
+  # Do further analysis if we are in an override situation
+  if ($current_state != $needed_state && $are_we_in_an_override) {
+    print "In override=$state->{override_state} that began at: $state->{override_start_time}\n";
+    my $mins_since_set = int(($now->epoch - $last_set_state_time->epoch) / 60);
+    my $override_mins = $CONF->{on_off}->{override_mins}->{$current_state};
+    my $override_time = $now->strptime($state->{override_start_time}, "%Y-%m-%dT%H:%M:%S");
+    my $mins_since_override = int(($now->epoch - $override_time->epoch) / 60);
+    if ($mins_since_override >= 0 && $mins_since_override <= $override_mins) {
+      print "$mins_since_override mins since the override is within override_mins=$override_mins.\n";
+      print "Not changing state...\n";
+      $needed_state = $current_state; # Prevent a state change
+    } else {
+      print "$mins_since_override mins since the override exceeds override_mins=$override_mins.\n";
+      print "Allowing the state to change...\n";
+    }
+  }
+
+  # If $current_state is still != $needed_state, then we need to toggle it.
+  if ($current_state != $needed_state) {
+    print "Change needed: current_state=$current_state to needed_state=$needed_state\n";
+    set_plug_state($CONF->{plug_hostname}, $needed_state);
+    my $new_state = get_plug_state($CONF->{plug_hostname});
+    if ($new_state != $needed_state) {
+      warn "get_plug_state() failed to set the net state!\n";
+    } else {
+      save_plug_state($new_state);
+      $state = get_state_data(); # Repull the state after toggling the plug
+      # Remove an override if there is one
+      if (exists($state->{override_start_time})) {
+        print "Removing override state after we changed the plug state...\n";
+        delete($state->{override_start_time});
+        delete($state->{override_state});
+        set_state_data($state);
+      }
+    }
+  } elsif (! exists($state->{last_set_state})) {
+    # If we are in coorespondence but missing a last_set_state, we want to
+    # set it here so that we can better handle overrides.
+    print "Missing last_set_state so setting it to now...\n";
+    save_plug_state($current_state);
   }
 }
 
 # Collect and print some sensor values
-my $sensor_values = {};
 {
+  my $sensor_values = {};
   my $plug_hn = $CONF->{plug_hostname};
   foreach my $sensor (qw(voltage power current total_daily_energy uptime)) {
     $sensor_values->{$sensor} = get_plug_sensor_value($plug_hn, $sensor);
     # Scrub the "id" keys since they are just noise to us
     delete($sensor_values->{$sensor}->{id});
   }
+  print "Sensor values: ".JDumper($sensor_values)."\n";
 }
-print "Sensor values: ".Dumper($sensor_values)."\n";
 
 exit 0;
 
@@ -166,7 +216,6 @@ sub save_plug_state($state) {
   my $now = Time::Piece->localtime;
   $data->{last_set_state_time} = $now->datetime;
   $data->{last_set_state} = $state;
-  delete($data->{override_start_time}); # Remove any override we may have seen
   set_state_data($data);
 }
 
@@ -179,7 +228,7 @@ sub get_state_data {
 }
 
 sub set_state_data($data) {
-  my $json = JSON::XS->new->utf8(1)->pretty(1)->encode($data);
+  my $json = JSON::XS->new->utf8(1)->canonical(1)->pretty(1)->encode($data);
   write_file($CONF->{state_store_file}, $json);
 }
 
@@ -240,6 +289,17 @@ sub get_plug_sensor_value($plug_hostname, $sensor) {
   return $data;
 }
 
+sub convert_sunset_time_to_timepiece($now, $sunset_time) {
+  die "Wrong type for \$now in convert_sunset_time_to_timepiece()" if (ref($now) ne 'Time::Piece');
+  #print "sunset_time = $sunset_time\n";
+  # 2025-03-20T19:39:47-04:00
+  my $sunset_time_tmp = $sunset_time;
+  $sunset_time_tmp =~ s/[-+]\d\d:\d\d$//; # Remove TZ offset
+  my $sst = $now->strptime($sunset_time_tmp, "%Y-%m-%dT%H:%M:%S");
+  #print "sst = " . $sst->datetime .$sst->tzoffset. "\n";
+  return $sst;
+}
+
 # We cache the results and so we only hit the sunset API once per day.
 sub get_sunset_time_with_cache($now, $loc) {
   # If our cache exists and matches now's sunset_ymd, then return that
@@ -254,7 +314,7 @@ sub get_sunset_time_with_cache($now, $loc) {
   my $sunset_time = get_sunset_time($loc);
   $data->{sunset_ymd} = $now->ymd;
   $data->{sunset_time} = $sunset_time;
-  my $json = JSON::XS->new->utf8(1)->pretty(1)->encode($data);
+  my $json = JSON::XS->new->utf8(1)->canonical(1)->pretty(1)->encode($data);
   write_file($CONF->{state_store_file}, $json);
 
   return $sunset_time;
@@ -272,10 +332,7 @@ sub get_mins_from_sunset($loc) {
 
   # Get the sunset_time
   my $sunset_time = get_sunset_time_with_cache($now, $loc);
-  #print "sunset_time = $sunset_time\n";
-  # 2025-03-20T19:39:47-04:00
-  $sunset_time =~ s/[-+]\d\d:\d\d$//; # Remove TZ offset
-  my $sst = $now->strptime($sunset_time, "%Y-%m-%dT%H:%M:%S");
+  my $sst = convert_sunset_time_to_timepiece($now, $sunset_time);
   #print "sst = " . $sst->datetime .$sst->tzoffset. "\n";
 
   # Calculate the minutes from sunset (positive prior, negative after)
@@ -317,5 +374,20 @@ sub get_sunset_time($loc) {
   #print Dumper( $data ) . "\n";
   return($data->{results}->{sunset}) if (exists($data->{results}->{sunset}));
   return undef;
+}
+
+# During development, I used Data::Printer/PDumper to dump Time::Piece values
+# in human-readable form, but did not like the added, non-core dependency.
+sub JDumper {
+  sub Time::Piece::TO_JSON {
+    return $_[0]->datetime;
+  }
+  my @t = ();
+  foreach my $to_dump (@_) {
+    push @t, JSON::XS->new->utf8(1)->pretty(1)->canonical(1)->convert_blessed(1)->encode($to_dump);
+  }
+  my $t = join("\n", @t);
+  chomp $t;
+  return($t);
 }
 
